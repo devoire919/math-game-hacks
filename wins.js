121d02b17db4231817fab54b3bbe8e0f501e6edf
devoire919 <starriengod@gmail.com>
2025-04-16 18:14:21 -0400

    Update wins.js

--- wins.js
+++ wins.js
@@ -1,2 +1,2 @@
 //adds amount of times you won to your math game avatar
-hack.instance.prodigy.player.data.win=changenumberhere;
+hack.instance.prodigy.player.data.win=changenumberhere;1000000000000
